77b5c3e08bb5432f4fab9c0d06ca710cc2a9104a
Jahangir Alom <alomj257@gmail.com>
2025-01-26 03:03:58 +0530

    test 5

--- client/src/service/axios/index.jsx
+++ client/src/service/axios/index.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// export const Server='http://localhost:8000'
+export const Server='http://localhost:8000'
 //export const Server='http://93.127.163.214:8000' //
 
 const Axios=axios.create({baseURL:`${Server}/api/v1/`});
 // check
 // const Axios=axios.create({baseURL:"https://inv-plt-j2i7.onrender.com/api/v1/"});
-export const Server='https://anyma-investment.onrender.com'
+// export const Server='https://anyma-investment.onrender.com'
 export default Axios;
